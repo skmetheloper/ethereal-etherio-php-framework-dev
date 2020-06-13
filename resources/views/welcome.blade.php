92a073033e6c11452aeea7585df315f5fe489a51
Shin Maung Maung <ethereal97@gmail.com>
2020-06-13 23:21:54 +0630

    Update and rename welcome.blade.phtml to welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.master')
+
+@route::has('login')
+{{-- This section will render when route 'login' is existed --}}
+<div class="section">
+    <div class="container">
+        <div class="wrapper">
+        @auth
+            <div class="card user status">
+                <div class="card title">
+                    <a class="card link underline">{{ user.name }}</a>
+                </div>
+                <div class="card message">
+                    <a class="card info tag">{{ user.role }}</a>
+                </div>
+            </div>
+            <a class="button outline logout" href="@route('logout')">{{ route.name }}</a>
+        @guest
+            <a class="button login" href="@route('login')">{{ route.name }}</a>
+            <a class="button clean register" href="@route('register')">Create an account</a>
+        @endauth
+        </div>
+    </div>
+</div>
+@endroute
+
+{{-- The contents without sections will store at 'content' section on default --}}
+<div class="section">
+    <div class="container">
+        
+    </div>
+</div>
