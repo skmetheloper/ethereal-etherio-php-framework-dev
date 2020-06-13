9d417534ab6fd16e2a27057c9fb897c58f3160a8
Shin Maung Maung <ethereal97@gmail.com>
2020-06-14 00:47:05 +0630

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,16 @@ Route::get('/', function () {
 Route::get('about', function () {
     return view('about');
 });
+
+Route::get('login', 'UserController@login');
+Route::post('login', 'UserController@auth');
+
+Route::get('register', 'UserController@register');
+Route::post('register', 'UserController@create');
+
+Route::auth();
+// The page that needs to be authorized
+
+Route::get('dashboard', 'UserController@dashboard');
+
+Route::post('logout', 'UserController@logout');
